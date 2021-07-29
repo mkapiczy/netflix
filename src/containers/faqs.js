9bf45402c0d24324e3179c9b1e6b0d3737969d95
Michal Kapiczynski <michal.kapiczynski@backstopdevelopment.com>
2021-07-29 23:33:01 +0300

    Fix accordion styling

--- src/containers/faqs.js
+++ src/containers/faqs.js
@@ -6,14 +6,13 @@ export function FaqsContainer() {
     return (
         <Accordion>
             <Accordion.Title>Frequently Asked Questions</Accordion.Title>
-            <Accordion.Frame>
-                {faqsData.map((faq) => (
-                    <Accordion.Item key={faq.id}>
-                        <Accordion.Header>{faq.header}</Accordion.Header>
-                        <Accordion.Body>{faq.body}</Accordion.Body>
-                    </Accordion.Item>
-                ))}
-            </Accordion.Frame>
+            {faqsData.map((faq) => (
+                <Accordion.Item key={faq.id}>
+                    <Accordion.Header>{faq.header}</Accordion.Header>
+                    <Accordion.Body>{faq.body}</Accordion.Body>
+                </Accordion.Item>
+            ))}
+            <Accordion.Item />
         </Accordion>
     );
 }
